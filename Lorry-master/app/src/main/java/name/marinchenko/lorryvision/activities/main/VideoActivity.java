e8ee952a1e3a27a67b0a5761facfab74aa0cf717
drill2010 <drill2010@yandex.ru>
2019-01-11 21:38:25 +0000

    added libvlc library
    project sync ok

--- Lorry-master/app/src/main/java/name/marinchenko/lorryvision/activities/main/VideoActivity.java
+++ Lorry-master/app/src/main/java/name/marinchenko/lorryvision/activities/main/VideoActivity.java
@@ -6,9 +6,12 @@ import android.os.Handler;
 import android.os.Message;
 import android.os.Messenger;
 import android.text.format.Formatter;
+import android.view.SurfaceView;
+import android.view.TextureView;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -19,11 +22,48 @@ import name.marinchenko.lorryvision.activities.ToolbarAppCompatActivity;
 import name.marinchenko.lorryvision.util.Initializer;
 import name.marinchenko.lorryvision.util.net.WifiAgent;
 
+import org.videolan.libvlc.IVLCVout;
+import org.videolan.libvlc.LibVLC;
+import org.videolan.libvlc.Media;
+import org.videolan.libvlc.MediaPlayer;
+
+
+
 import static name.marinchenko.lorryvision.services.NetScanService.MSG_DISCONNECTED;
 import static name.marinchenko.lorryvision.services.NetScanService.MSG_RETURN_TO_MAIN;
 
-//ffff
+
 public class VideoActivity extends ToolbarAppCompatActivity {
+
+
+    private static final boolean USE_SURFACE_VIEW = true;
+    private static final boolean ENABLE_SUBTITLES = true;
+    private static final String TAG = "JavaActivity";
+    private static final int SURFACE_BEST_FIT = 0;
+    private static final int SURFACE_FIT_SCREEN = 1;
+    private static final int SURFACE_FILL = 2;
+    private static final int SURFACE_16_9 = 3;
+    private static final int SURFACE_4_3 = 4;
+    private static final int SURFACE_ORIGINAL = 5;
+    private static int CURRENT_SIZE = SURFACE_BEST_FIT;
+
+    private FrameLayout mVideoSurfaceFrame = null;
+    private SurfaceView mVideoSurface = null;
+    private SurfaceView mSubtitlesSurface = null;
+    private TextureView mVideoTexture = null;
+    private View mVideoView = null;
+
+    private View.OnLayoutChangeListener mOnLayoutChangeListener = null;
+
+    private LibVLC mLibVLC = null;
+    private MediaPlayer mMediaPlayer = null;
+    private int mVideoHeight = 0;
+    private int mVideoWidth = 0;
+    private int mVideoVisibleHeight = 0;
+    private int mVideoVisibleWidth = 0;
+    private int mVideoSarNum = 0;
+    private int mVideoSarDen = 0;
+
 
     public static final int JUMP_DELAY = 500;
 
