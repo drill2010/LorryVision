0db49a68e9c0d8b96d82abcc253fd48e98feac08
drill2010 <drill2010@yandex.ru>
2019-01-25 08:25:07 +0000

    autoconnect already disconnected net fixed

--- Lorry-master/app/src/main/java/name/marinchenko/lorryvision/services/NetScanService.java
+++ Lorry-master/app/src/main/java/name/marinchenko/lorryvision/services/NetScanService.java
@@ -260,9 +260,13 @@ public class NetScanService extends Service {
 
     private boolean alreadyAutoConnected(Net net){
         // checks if lorry net is already automatically connected by phone, during wifi service enable process
+        if (net==null) return false;
         final Net connected =  this.netBuffer.getConnectedNet();
         if (connected !=null) return false;   // this means, that lorry net is already connected by application
         else{
+
+            if (this.autoConnect && net.wasConnected()) return false;
+
             if (WifiAgent.connectedTo(this,
                     WifiConfig.formatSsid(net.getSsid()))
                     )
@@ -286,6 +290,7 @@ public class NetScanService extends Service {
             sendMsgСonnectEnd(MSG_СONNECT_END_OK);
             return;
         }
+
         if (canConnect(net)) connect(net);
     }
 
